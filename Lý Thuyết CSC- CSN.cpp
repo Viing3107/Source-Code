65fdd790b6eb6402e8b4a3e2ff49e54c8a0961eb
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-05-19 13:33:29 +0700

    Add files via upload

---
+++ Lý Thuyết CSC- CSN.cpp
@@ -0,0 +1,58 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 11:03:08 - 19/05/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+int t, n, c, answer, u1;
+
+int binaryExponentiation(int a, int n){
+    int res = 1;
+    for(; n; n >>= 1, a = (a * a) % MOD){
+        if (n & 1) res = (res * a) % MOD;
+    }
+    return res;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")){
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+    
+    cin >> t;
+    while (t--){
+        cin >> n >> c;
+        u1 = c + 2;
+        answer = (u1 * binaryExponentiation(3, n - 1)) % MOD;
+        cout << answer - 2 << endl;
+    }
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/fc076_number
