ddf985fdfe0edbda5c7598ec163187353b06ed87
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-05-24 22:13:29 +0700

    Add files via upload

---
+++ LIS.cpp
@@ -0,0 +1,52 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 22:09:58 - 24/05/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+
+using namespace std;
+
+const int N     = 2e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+int n, a[N], result;
+set<int> s;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")){
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+    
+    cin >> n;
+    forUp(i, 1, n, 1) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    forUp(i, 1, n, 1){
+        s.insert(a[i]);
+        result += s.size();
+    }
+    cout << result;
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/bedao_r07_lis
