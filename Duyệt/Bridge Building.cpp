d2549d8c109805907a645f17e422b95302a1b99d
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-06-23 20:35:56 +0700

    Add files via upload

---
+++ Duyệt/Bridge Building.cpp
@@ -0,0 +1,53 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 20:33:10 - 23/06/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+#define forEach(it, v)          for(typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+int n, a[N];
+double result;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")){
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> n;
+    forUp(i, 1, n, 1) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    forUp(i, 1, n - 1, 1) result += sqrt((a[i + 1] - a[i]) * (a[i + 1] - a[i]) + 1);
+    cout << fixed << setprecision(6) << result;
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/codetour24_c3_b
