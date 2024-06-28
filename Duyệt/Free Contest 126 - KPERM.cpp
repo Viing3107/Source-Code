25a7ef231e190648b21f37160f2001c7dc403267
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-06-28 13:51:45 +0700

    Add files via upload

---
+++ Duyệt/Free Contest 126 - KPERM.cpp
@@ -0,0 +1,55 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 13:43:15 - 28/06/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+#define forEach(it, v)          for(typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+int n, k, last, state;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")){
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> n >> k;
+    forUp(i, 1, n - k, 1) cout << i << " ";
+    last = n - k;
+    for (int i = k; i >= 1; i--, state ^= 1){
+        if (!state) last += i;
+        else last -= i;
+        cout << last << " ";
+    }
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/fc126_kperm
