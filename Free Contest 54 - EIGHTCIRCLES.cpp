3b52f8d571beced2881a237a9efebb67996c3199
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-06-29 01:09:35 +0700

    Add files via upload

---
+++ Free Contest 54 - EIGHTCIRCLES.cpp
@@ -0,0 +1,50 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 01:06:49 - 29/06/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+#define forEach(it, v)          for(typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+double x;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")){
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> x;
+    double tmp = sqrt(3) + 1 + sqrt(2);
+    cout << fixed << setprecision(4) << sqrt(x / (2 * tmp * tmp));
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/fc054_eightcircles
