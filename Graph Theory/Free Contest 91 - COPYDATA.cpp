d4ce5597dfbdfde6de88951bd25210a6bc879d7c
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-06-30 13:48:29 +0700

    Free Contest 91 - COPYDATA.cpp

--- Graph Theory/Free Contest 91 - COPYDATA.cpp
+++ Graph Theory/Free Contest 91 - COPYDATA.cpp
@@ -1,24 +1,35 @@
-#include						<bits/stdc++.h>
-#define int 					long long
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 13:46:46 - 30/06/2024
+
+*/
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
 #define task                    "test"
+#define forUp(i, l, r, step)    for(int i = l; i <= r; i += step)
+#define forDown(i, l, r, step)  for(int i = l; i >= r; i += step)
+#define forEach(it, v)          for(typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
 
 using namespace std;
 
-const int N 	= 3e5 + 5;
-const int INF	= 0x3f3f3f3f3f3f3f3f;
+const int N     = 3e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
 
 int n, a, b, ans = INF, trace[N], f[N], x, y;
-vector<int> adj[N], p;
+vector<int> adj[N], path;
 
 /*
 	f[u] : thời gian nhỏ nhất để thăm hết các con của u
 */
 
-void get(int u, int par) {
+void getPath(int u, int par) {
 	for (auto v : adj[u]) {
 		if (v == par) continue;
 		trace[v] = u;
-		get(v, u);
+		getPath(v, u);
 	}
 }
 
@@ -40,17 +51,17 @@ void dfs(int u, int par, int x) {
 	for (int i = 0; i < d.size(); i++) f[u] = max(f[u], d[i] + (i + 1));
 }
 
-signed main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     if (fopen(task".inp", "r")){
         freopen(task".inp", "r", stdin);
         freopen(task".out", "w", stdout);
     }
-	
-	cin >> n >> a >> b;
+
+    cin >> n >> a >> b;
 	for (int i = 1; i <= n - 1; i++) {
 		int u, v;
 		cin >> u >> v;
@@ -59,19 +70,14 @@ signed main() {
 	}
 	
 	// vector p : đường đi từ b -> a
-	get(a, 0);
-	int tmp = b;
-	while (tmp != a) {
-		p.push_back(tmp);
-		tmp = trace[tmp];
-	}
-	p.push_back(a);
+	getPath(a, 0);
+	for (int u = b; u; u = trace[u]) path.push_back(u);
 
     // chặt nhị phân
-	int l = 1, r = p.size();
+	int l = 1, r = path.size();
 	while (l <= r) {
 		int m = l + r >> 1;
-		x = p[m - 1], y = p[m];
+		x = path[m - 1], y = path[m];
 		/*
 			bỏ qua cạnh (p[m - 1], p[m])
 			ý tưởng là tính riêng từng thành phần liên thông, rồi kết quả là min của max(f[a], f[b]) trong mọi trường hợp
@@ -86,9 +92,18 @@ signed main() {
 		else r = m - 1; 
 	}
 	cout << ans;
-	
-	return 0;
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
 }
 
+/*
 
-// https://oj.vnoi.info/problem/fc091_copydata
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://hackmd.io/@A-QGjynMR_uo0kpAG-kK8Q/S1ZzRcpS0
