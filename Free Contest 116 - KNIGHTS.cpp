8c2b0d0b1ea494681aee117bc76588675d880850
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-07-10 23:01:21 +0700

    Add files via upload

---
+++ Free Contest 116 - KNIGHTS.cpp
@@ -0,0 +1,56 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 22:58:01 - 10/07/2024
+
+*/
+
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define task                    "test"
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define endl                    "\n"
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+int n, q, a, b, m;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> q >> n;
+    m = 1ll << (n - 1);
+    while (q--) {
+        cin >> a >> b;
+        if (a > b) swap(a, b);
+        cout << ((a <= m && b > m) ? "yes\n" : "no\n");
+    }
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/fc116_knights
