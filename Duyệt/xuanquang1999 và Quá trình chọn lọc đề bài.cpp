646670ac44271f784e0621d22c4c4968848ead38
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-07-20 11:19:40 +0700

    Add files via upload

---
+++ Duyệt/xuanquang1999 và Quá trình chọn lọc đề bài.cpp
@@ -0,0 +1,63 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 11:08:58 - 20/07/2024
+
+*/
+
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define endl                    "\n"
+#define task                    "test"
+#define all(v)                  (v).begin(), (v).end()
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int N             = 2e5 + 5;
+const int MOD           = 1e9 + 7;
+const int INF           = 0x3f3f3f3f3f3f3f3f;
+
+int t, n, a[N];
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        forUp(i, 1, n, 1) cin >> a[i];
+        int result = 0;
+        forDown(i, n - 1, 1, -1) {
+            if (a[i] < a[i + 1]) {
+                a[i] += a[i + 1];
+                result++;
+            }
+        }
+        cout << n - result << endl;
+    }
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// https://oj.vnoi.info/problem/vnoicup23_final_a
