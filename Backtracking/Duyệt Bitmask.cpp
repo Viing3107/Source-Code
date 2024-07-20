2780d35ef36c6d75f1f3b6a1fc73fc59b3ea4da4
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2024-07-20 10:14:38 +0700

    Add files via upload

---
+++ Backtracking/Duyệt Bitmask.cpp
@@ -0,0 +1,56 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 07:20:24 - 18/07/2024
+
+*/
+
+#include                        <bits/stdc++.h>
+#define int                     long long
+#define double                  long double
+#define endl                    "\n"
+#define task                    "test"
+#define all(v)                  (v).begin(), (v).end()
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int N     = 1e5 + 5;
+const int MOD   = 1e9 + 7;
+const int INF   = 0x3f3f3f3f3f3f3f3f;
+
+
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < (1 << n); i++) {
+        forUp(j, 0, n - 1, 1) cout << ((i >> j) & 1) << " ";
+        cout << endl;
+    }
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " .\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 VOI
+
+*/
+
+// 
