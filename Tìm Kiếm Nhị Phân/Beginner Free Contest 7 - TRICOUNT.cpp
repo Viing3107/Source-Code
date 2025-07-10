e8ba88cf7ce41bfecb3aed0ae3378af40d3ec94f
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2025-07-10 14:27:55 +0700

    Add files via upload

---
+++ Tìm Kiếm Nhị Phân/Beginner Free Contest 7 - TRICOUNT.cpp
@@ -0,0 +1,64 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+
+*/
+
+#include                        <bits/stdc++.h>
+#define endl                    "\n"
+#define task                    "test"
+#define int                     long long
+#define double                  long double
+#define MASK(i)                 (1ll << (i))
+#define BIT(i, n)               ((n) >> (i) & 1)
+#define SZ(v)                   (int) (v).size()
+#define ALL(v)                  (v).begin(), (v).end()
+#define LOG(x)                  (63 - __builtin_clzll(x))
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof v.begin() it = v.begin(); it != v.end(); it++)
+
+using namespace std;
+
+const int       N           = 1e5 + 5;
+const int       BASE        = 31;
+const int       MOD         = 1e9 + 7;
+const int       BLOCK_SIZE  = sqrt(N) + 5;
+const int       INF         = 0x3f3f3f3f3f3f3f3f;
+const double    EPS         = 1e-6;
+
+int n, a[N], res;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> n;
+    forUp (i, 1, n, 1) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    forUp (i, 1, n - 2, 1) forUp (j, i + 2, n, 1) {
+        int l = lower_bound(a + i + 1, a + j, a[j] + 1 - a[i]) - a;
+        res += j - l;
+    }
+    cout << res;
+
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 Viing3107
+
+*/
+
+// https://oj.vnoi.info/problem/fcb007_tricount
