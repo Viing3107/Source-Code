1dcd343d62fb0ca051300f0f3f12b61566682e02
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2025-07-10 17:32:38 +0700

    Add files via upload

---
+++ Tìm Kiếm Nhị Phân/ Distinctive Number.cpp
@@ -0,0 +1,83 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+
+*/
+
+#include                        <bits/stdc++.h>
+#define endl                    "\n"
+#define task                    "test"
+#define int                     long long
+#define double                  long double
+#define MASK(i)                 (1ll << (i))
+#define BIT(i, n)               ((n) >> (i) & 1)
+#define SZ(v)                   (int) (v).size()
+#define ALL(v)                  (v).begin(), (v).end()
+#define LOG(x)                  (63 - __builtin_clzll(x))
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof v.begin() it = v.begin(); it != v.end(); it++)
+
+using namespace std;
+
+const int       N           = 5e5 + 5;
+const int       BASE        = 31;
+const int       MOD         = 1e9 + 7;
+const int       BLOCK_SIZE  = sqrt(N) + 5;
+const int       INF         = 0x3f3f3f3f3f3f3f3f;
+const double    EPS         = 1e-6;
+
+int n, a[N], rollback[N];
+vector<int> v;
+set<int> mark;
+bitset<N> removed;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> n;
+    forUp (i, 1, n, 1) {
+        cin >> a[i];
+        v.push_back(a[i]);
+    }
+    sort(ALL(v));
+    v.resize(unique(ALL(v)) - v.begin());
+    forUp (i, 1, n, 1) {
+        int x = lower_bound(ALL(v), a[i]) - v.begin() + 1;
+        rollback[x] = a[i];
+        a[i] = x;
+    }
+//    forUp (i, 1, n, 1) cout << a[i] << " \n"[i == n];
+//    forUp (i, 1, n, 1) cout << rollback[i] << " \n"[i == n];
+    forUp (i, 1, n, 1) {
+        auto itr = mark.lower_bound(a[i]);
+        if (*itr == a[i]) {
+            removed.set(a[i]);
+            mark.erase(itr);
+        }
+        else if (!removed.test(a[i])) mark.insert(a[i]);
+        if (SZ(mark)) cout << rollback[*mark.begin()] << endl;
+        else cout << -1 << endl;
+    }
+
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1 Viing3107
+
+*/
+
+// https://oj.vnoi.info/problem/icpc21_regional_d
