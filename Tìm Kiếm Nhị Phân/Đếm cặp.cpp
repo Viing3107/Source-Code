ec93ebf2bda6b014d7c677f8e9745b4259561ce4
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2025-10-04 00:04:38 +0700

    Add files via upload

---
+++ Tìm Kiếm Nhị Phân/Đếm cặp.cpp
@@ -0,0 +1,68 @@
+/*
+    Author : Do Quang Vinh
+    Kim Son A High School - Ninh Binh
+    Created : 23:55:48 - 03/10/2025
+
+*/
+
+#include                        <bits/stdc++.h>
+#define endl                    "\n"
+#define task                    "test"
+#define int                     long long
+#define double                  long double
+#define MASK(i)                 (1ll << (i))
+#define BIT(i, n)               ((n) >> (i) & 1)
+#define SZ(v)                   (int) (v).size()
+#define ALL(v)                  (v).begin(), (v).end()
+#define LOG(x)                  (63 - __builtin_clzll(x))
+#define forUp(i, l, r, step)    for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step)  for (int i = (l); i >= (r); i += step)
+#define forEach(it, v)          for (typeof (v).begin() it = (v).begin(); it != (v).end(); it++)
+
+using namespace std;
+
+const int       BASE        = 311;
+const int       BLOCK_SIZE  = 320;
+const int       N           = 1e5 + 5;
+const int       VAL         = 1e6 + 6;
+const int       MOD         = 1e9 + 7;
+const int       INF         = 0x3f3f3f3f3f3f3f3f;
+const double    EPS         = 1e-6;
+
+int n, l, r, a[N];
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task".inp", "r")) {
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    }
+
+    cin >> n >> l >> r;
+    forUp (i, 1, n, 1) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    int res = 0;
+    forUp (i, 1, n, 1) {
+        int ll = lower_bound(a + i + 1, a + n + 1, l - a[i]) - a;
+        int rr = upper_bound(a + i + 1, a + n + 1, r - a[i]) - a - 1;
+        if (ll <= rr) res += rr - ll + 1;
+    }
+    cout << res;
+    
+    cerr << "Time elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+    return (0 ^ 0);
+}
+
+/*
+
+    /\_/\
+   (= ._.)
+   />0   \>1
+
+*/
+
+// https://marisaoj.com/problem/80
