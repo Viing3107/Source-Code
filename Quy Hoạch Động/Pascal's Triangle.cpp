bd46723a41bfb1732ed62838723bead6b5ce7604
Viing3107 <168270572+Viing3107@users.noreply.github.com>
2025-11-15 06:29:42 +0700

    Add files via upload

---
+++ Quy Hoạch Động/Pascal's Triangle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> row = {1};
+        res.push_back(row);
+        for (int i = 1; i < numRows; i++) {
+            vector<int> row = {1};
+            for (int j = 1; j < i; j++) {
+                row.push_back(res[i - 1][j - 1] + res[i - 1][j]);
+            }
+            row.push_back(1);
+            res.push_back(row);
+        }
+        return res;
+    }
+};
+
+// https://leetcode.com/problems/pascals-triangle/description/
