73a0bcd1987b9f18682c984b25fb6f18a71225a4
Do Quang Vinh <168270572+Viing3107@users.noreply.github.com>
2026-08-25 20:53:47 +0700

    Add files via upload

---
+++ Graph Theory/Đế chế.cpp
@@ -0,0 +1,129 @@
+/*
+    Author: Do Quang Vinh
+    VNU University of Engineering and Technology, Hanoi, Vietnam
+    Created: 20:13:10 25/08/2026
+
+*/
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define task "test"
+#define int long long
+#define double long double
+#define gcd(x, y) __gcd(x, y)
+#define MASK(i) (1ll << (i))
+#define SZ(v) (int)(v).size()
+#define BIT(i, n) ((n) >> (i) & 1)
+#define lcm(x, y) x / gcd(x, y) * y
+#define ALL(v) (v).begin(), (v).end()
+#define LOG(x) (63 - __builtin_clzll(x))
+#define forUp(i, l, r, step) for (int i = (l); i <= (r); i += step)
+#define forDown(i, l, r, step) for (int i = (l); i >= (r); i += step)
+
+using namespace std;
+
+const int LEVEL = 20;
+const int BASE = 311;
+const int BLOCK_SIZE = 320;
+const double EPS = 1e-6;
+const int N = 1e5 + 5;
+const int VAL = 1e6 + 6;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+
+struct Point {
+    int x, y, z, id;
+};
+
+int n;
+Point a[N];
+
+struct DisjointSetUnion {
+    int par[N], sz[N];
+
+    void build() {
+        forUp (u, 1, n, 1) {
+            par[u] = u;
+            sz[u] = 1;
+        }
+    }
+
+    int find(int u) {
+        return u == par[u] ? u : par[u] = find(par[u]);
+    }
+
+    bool join(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) return false;
+        if (sz[u] < sz[v]) swap(u, v);
+        par[v] = u;
+        sz[u] += sz[v];
+        return true;
+    }
+} dsu;
+
+struct Edge {
+    int u, v, w;
+
+    friend bool operator < (const Edge &u, const Edge &v) {
+        return u.w < v.w;
+    }
+};
+
+vector<Edge> edges, MST;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    if (fopen(task ".inp", "r")) {
+        freopen(task ".inp", "r", stdin);
+        freopen(task ".out", "w", stdout);
+    }
+
+    cin >> n;
+    forUp (i, 1, n, 1) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        a[i] = {x, y, z, i};
+    }
+    
+    sort(a + 1, a + n + 1, [&] (const Point &u, const Point &v) {
+        return u.x < v.x;
+    });
+    forUp (i, 1, n - 1, 1) edges.push_back({a[i].id, a[i + 1].id, abs(a[i + 1].x - a[i].x)});
+
+    sort(a + 1, a + n + 1, [&] (const Point &u, const Point &v) {
+        return u.y < v.y;
+    });
+    forUp (i, 1, n - 1, 1) edges.push_back({a[i].id, a[i + 1].id, abs(a[i + 1].y - a[i].y)});
+
+    sort(a + 1, a + n + 1, [&] (const Point &u, const Point &v) {
+        return u.z < v.z;
+    });
+    forUp (i, 1, n - 1, 1) edges.push_back({a[i].id, a[i + 1].id, abs(a[i + 1].z - a[i].z)});
+    sort(ALL(edges));
+    dsu.build();
+    int res = 0;
+    for (int i = 0; i < SZ(edges) && SZ(MST) < n - 1; i++) {
+        auto [u, v, w] = edges[i];
+        if (dsu.join(u, v)) {
+            res += w;
+            MST.push_back({u, v, w});
+        }
+    }
+    cout << res;
+
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+    return (0 ^ 0);
+}
+
+/*   /\_/\
+    (= ._.)
+    />0   \>1
+*/
+
+// https://oj.vnoi.info/problem/vr_mst_deche
